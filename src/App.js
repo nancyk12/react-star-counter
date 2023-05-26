471fde4085ec2a1df38eb0b324c9ab2ccf9e9877
nancyk12 <nancyk12@hotmail.com>
2023-05-25 23:32:08 -0400

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from 'react';
+import "./App.css";
 
-function App() {
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const handleAdd = () => {
+    setCount(prevCount => prevCount + 1);
+  };
+
+  const handleRemove = () => {
+    if (count > 0) {
+      setCount(prevCount => prevCount - 1);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div>
+      <header className="header">
+       
+        <img className="logo" src="https://www.thesprucecrafts.com/thmb/SwK_Y20sbljJG4TObLqnzHDv6sY=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/shooting-star-1--587e4d935f9b584db3e0d71e.png" alt="Logo"/>
+    
+        <h1 className="app-name">Count the Stars</h1>
+        <div className="menu">Menu</div>
       </header>
+      <div className="counter">
+        <button className="subtractButton" onClick={handleRemove}>Remove 1</button>
+        <span>{count}</span>
+        <button className="addButton" onClick={handleAdd}>Add 1</button>
+      </div>
+      <div className="stars-container">
+        {Array.from({ length: count }).map((_, index) => (
+          <div className="star-box" key={index}>
+            <span className="star-icon">&#9733;</span>
+          </div>
+        ))}
+      </div>
     </div>
   );
-}
+};
 
 export default App;
